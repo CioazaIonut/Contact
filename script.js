fe3498e346a0cbc9037588273b4b151240dc0d36
CioazaIoan <cioazai@yahoo.com>
2024-06-30 12:50:48 +0300

    primul

---
+++ script.js
@@ -0,0 +1,19 @@
+const buton = document.querySelector(".button-class");
+
+
+const singurulButon = () => {
+
+
+}
+
+buton.addEventListener("click",singurulButon);
+
+//alert("Oare functioneaza?")
+
+
+/* 
+de facut partea de mobil la design          Facut
+de facut design-ul
+culorile cu litere mici                     Facut
+
+*/
